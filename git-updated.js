984a7069c6401e22653e10115f58fd614ae7d3a8
frapporti <effedue@gmail.com>
2023-12-09 13:57:59 +0100

    less insecure

--- git-updated.js
+++ git-updated.js
@@ -5,37 +5,32 @@ const path = require('path');
 async function updateFileDates() {
     try {
         const git = simpleGit();
-
-        // Check if we are in a Git repository
         const isRepo = await git.checkIsRepo();
-        if (!isRepo) throw new Error("Not a Git repository.");
 
-        // Find the root directory of the Git repository
+        if (!isRepo) {
+            throw new Error("Not a Git repository.");
+        }
+
         const repoRoot = await git.revparse(['--show-toplevel']);
-
-        // Fetch the log for all files
         const log = await git.log();
         const fileDates = {};
 
         log.all.forEach(commit => {
             commit.files.forEach(file => {
-                if (!fileDates[file]) {
+                // Only update if the file's date is newer
+                if (!fileDates[file] || fileDates[file] < commit.date) {
                     fileDates[file] = commit.date;
                 }
             });
         });
 
-        // Define the path for the JSON file relative to the repository root
         const jsonFilePath = path.join(repoRoot, '.git-updated', 'file_dates.json');
-
-        // Create the .git-updated directory if it doesn't exist
         fs.mkdirSync(path.dirname(jsonFilePath), { recursive: true });
-
-        // Write the dates to the JSON file
         fs.writeFileSync(jsonFilePath, JSON.stringify(fileDates, null, 2));
-        console.log('File dates updated in .git-updated/file_dates.json');
+
+        console.log('git-updated: File dates updated in .git-updated/file_dates.json');
     } catch (error) {
-        console.error('Error updating file dates:', error);
+        console.error('git-updated: Error updating file dates:', error);
     }
 }
 
--- postinstall.js
+++ postinstall.js
@@ -1,48 +1,25 @@
 const fs = require('fs');
 const path = require('path');
-const execSync = require('child_process').execSync;
 
-function findProjectRoot(currentDir) {
-    if (fs.existsSync(path.join(currentDir, '.git'))) {
-        // Found the .git directory, return the current directory
-        return currentDir;
-    }
-
-    const parentDir = path.resolve(currentDir, '..');
-    if (parentDir === currentDir) {
-        // Reached the filesystem root without finding .git
-        return null;
-    }
-
-    return findProjectRoot(parentDir);
-}
-
-const projectRoot = findProjectRoot(__dirname);
-if (!projectRoot) {
-    console.error('Unable to find Git repository root. git-updated requires a Git repository.');
-    process.exit(1);
-}
-
-const huskyDir = path.join(projectRoot, '.husky');
-const prePushHook = path.join(huskyDir, 'pre-push');
-const commandToAdd = "npx git-updated";
+const packageJsonPath = path.join(process.cwd(), 'package.json');
+const packageJson = require(packageJsonPath);
 
 function setupHusky() {
-    if (!fs.existsSync(huskyDir)) {
-        // Husky not installed, proceed with installation
-        execSync('npx husky install', { cwd: projectRoot });
-    }
+    const huskyConfig = packageJson['husky'] || {};
+    const hooks = huskyConfig['hooks'] || {};
 
-    if (fs.existsSync(prePushHook)) {
-        // Append to existing pre-push hook
-        const hookContent = fs.readFileSync(prePushHook, 'utf-8');
-        if (!hookContent.includes(commandToAdd)) {
-            fs.appendFileSync(prePushHook, `\n${commandToAdd}`);
-        }
-    } else {
-        // Create a new pre-push hook
-        execSync(`npx husky add .husky/pre-push "${commandToAdd}"`, { cwd: projectRoot });
-    }
+    // Add or modify pre-push hook
+    const prePushScript = 'node ./node_modules/git-updated/git-updated.js';
+    hooks['pre-push'] = hooks['pre-push'] ? `${hooks['pre-push']} && ${prePushScript}` : prePushScript;
+
+    // Update package.json
+    packageJson['husky'] = {...huskyConfig, hooks};
+    fs.writeFileSync(packageJsonPath, JSON.stringify(packageJson, null, 2));
+    console.log('git-updated: Husky pre-push hook configured.');
 }
 
-setupHusky();
+if (fs.existsSync(packageJsonPath)) {
+    setupHusky();
+} else {
+    console.error('git-updated: No package.json found. Please ensure you are in the root directory of your project.');
+}
