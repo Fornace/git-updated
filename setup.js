406a2f5720734bc7d6741466609875c599cf5594
frapporti <effedue@gmail.com>
2023-12-10 05:44:57 +0100

    here

--- setup.js
+++ setup.js
@@ -1,13 +1,16 @@
 // setup.js
+console.warn("postcd -install script")
+
 const fs = require('fs');
 const path = require('path');
-const setHooks = require('simple-git-hooks');
-console.warn("post-install script")
-const hooks = {
+const {setHooks} = require('simple-pre-commit')
+
+
+const hooks2 = {
   "post-commit": "node ./node_modules/git-updated/updateJson.js"
 };
 
-setHooks(hooks).then(() => {
+setHooks(hooks2).then(() => {
   console.log('Git Updated hooks set successfully.');
 }).catch((error) => {
   console.error('Error setting Git Updated hooks:', error);
